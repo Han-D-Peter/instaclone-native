07d1f05837558ae5f085dfea42b967bb6bb435f3
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-12-22 18:55:27 +0900

    navigation

---
+++ navigators/LoggedOutNav.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import CreateAccount from "../screens/CreateAccount";
+import LogIn from "../screens/LogIn";
+import Welcome from "../screens/Welcome";
+
+const Stack = createStackNavigator();
+
+export default function LoggedOutNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+    </Stack.Navigator>
+  );
+}
