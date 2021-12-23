9342b460f546f38c5bb42508bd61414954cbaf1d
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-12-23 17:42:26 +0900

    apollo setup

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,10 +1,12 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
 import styled from "styled-components/native";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 import { TextInput } from "../components/auth/AuthShared";
 
 export default function CreateAccount() {
+  const { register, handleSubmit, setValue } = useForm();
   const lastNameRef = useRef();
   const usernameRef = useRef();
   const emailRef = useRef();
@@ -13,47 +15,68 @@ export default function CreateAccount() {
     nextOne?.current?.focus();
   };
 
-  const onDone = () => {
-    alert("done!");
+  const onValid = data => {
+    console.log(data);
   };
+
+  useEffect(() => {
+    register("firstName", { required: true });
+    register("lastName", { required: true });
+    register("username", { required: true });
+    register("email", { required: true });
+    register("password", { required: true });
+  }, [register]);
 
   return (
     <AuthLayout>
       <TextInput
         placeholder="First Name"
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("firstName", text)}
         returnKeyType="next"
         onSubmitEditing={() => onNext(lastNameRef)}
+        autoCapitalize={"none"}
       />
       <TextInput
         ref={lastNameRef}
         placeholder="Last Name"
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("lastName", text)}
         returnKeyType="next"
         onSubmitEditing={() => onNext(usernameRef)}
       />
       <TextInput
         placeholder="Username"
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("username", text)}
         returnKeyType="next"
         onSubmitEditing={() => onNext(emailRef)}
+        autoCapitalize={"none"}
       />
       <TextInput
         placeholder="Email"
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("email", text)}
         keyboardType="email-address"
         returnKeyType="next"
         onSubmitEditing={() => onNext(passwordRef)}
+        autoCapitalize={"none"}
       />
       <TextInput
         placeholder="Password"
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("password", text)}
         secureTextEntry
         returnKeyType="done"
         lastOne={true}
-        nSubmitEditing={onDone}
+        onSubmitEditing={handleSubmit(onValid)}
+        autoCapitalize={"none"}
       />
-      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+      <AuthButton
+        text="Create Account"
+        loading
+        onPress={handleSubmit(onValid)}
+      />
     </AuthLayout>
   );
 }
--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 import { TextInput } from "../components/auth/AuthShared";
 
 export default function LogIn({ navigation }) {
+  const { register, handleSubmit, setValue } = useForm();
+  const passwordRef = useRef();
+
+  const onNext = nextOne => {
+    nextOne?.current?.focus();
+  };
+
+  const onValid = data => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    register("username", { required: true });
+    register("password", { required: true });
+  }, [register]);
+
   return (
     <AuthLayout>
       <TextInput
         placeholder="Username"
         returnKeyType="next"
+        autoCapitalize={"none"}
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onSubmitEditing={() => onNext(passwordRef)}
+        onChangeText={text => setValue("username", text)}
       />
       <TextInput
+        ref={passwordRef}
         placeholder="Password"
         secureTextEntry
         returnKeyType="done"
+        autoCapitalize={"none"}
         lastOne={true}
         placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onSubmitEditing={() => handleSubmit(onValid)}
+        onChangeText={text => setValue("password", text)}
+      />
+      <AuthButton
+        text="Log In"
+        disabled={true}
+        onPress={handleSubmit(onValid)}
       />
     </AuthLayout>
   );
