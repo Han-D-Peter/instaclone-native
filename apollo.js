e7e58887c6de13d2ba05ea6438cc44142d353220
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-12-23 22:28:20 +0900

    native localstorage

--- apollo.js
+++ apollo.js
@@ -1,6 +1,17 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
+
+export const logUserIn = async token => {
+  await AsyncStorage.multiSet([
+    ["token", token],
+    ["loggedIn", "yes"],
+  ]);
+  isLoggedInVar(true);
+  tokenVar(token);
+};
 
 const client = new ApolloClient({
   uri: "https://pretty-falcon-34.loca.lt/graphql",
